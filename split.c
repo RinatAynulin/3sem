5130f9e1fe287305b6db563ba5a84927ae5d20ac
VladMiryaha <vlad.miryaha@gmail.com>
2015-09-17 00:29:20 +0300

    fix it

--- split.c
+++ split.c
@@ -3,7 +3,18 @@
 #include <malloc.h>
 
 void Split(char* string, char* delimiters, char*** tokens_ptr, int* tokensCount) {
-    int str_num = 30;
+  /*
+   * FIXIT:
+   * Вынесите выделение памяти вне ф-и split. Почему? Допустим вы хотите оформить ф-ю в отдельную библиотеку, тогда пользователь увидит только
+   * сигнатуру ф-и. Ему надо будет догадаться каким-то образом, что ему память выделть не надо, но освободить он её должен. Это не круто, поэтому если вы освободаете память, то вы и должны были её выделить.
+   * По крайней мере так делать крайне желательно.
+   */
+  
+    /*
+     * лучше явно подчёркивать, то данная переменная является константой с помощью слова const
+     */
+  
+    const int str_num = 30;
     *tokens_ptr = (char**) calloc(str_num, sizeof(char*));
     char** tokens = *tokens_ptr;
     char* cur_token = NULL;
@@ -17,6 +28,11 @@ void Split(char* string, char* delimiters, char*** tokens_ptr, int* tokensCount)
     tokens[*tokensCount] = cur_token;
 }
 
+/*
+ * Очень хорошо, что вы сделали дополнительную ф-ю для вывода результата с понятным названием. 
+ * Это делает код самодокументируемым.
+ */
+
 void PrintTokens (char** tokens, int tokensCount) {
     int i = 0;
 
@@ -25,6 +41,10 @@ void PrintTokens (char** tokens, int tokensCount) {
 
 }
 int main() {
+    /*
+     * FIXIT:
+     * Надо объявить две константы MaxStringSize и MaxDelimetersCount вместо магических чисел 256
+     */
     char* string = (char*)malloc(sizeof(char) * 256);
     char* delimiters = (char*)malloc(sizeof(char) * 256);
     char **tokens = NULL;
@@ -36,9 +56,19 @@ int main() {
     Split(string, delimiters, &tokens, &tokensCount);
     PrintTokens(tokens, tokensCount);
     printf("%d", tokensCount);
+    /*
+     * FIXIT:
+     * В каком месте вы выделяли память под tokens[i]?
+     * Вероятно не выделяли явно. Тогда где хранятся слова? Слова хранятся непосредственно в string (метод strtok её модифицирует, добавляя в нужные места символы конца строки).
+     * Из-за того, что вы пытаетесь освободить память, которую не выделяли, у меня, например, программа падает с segmentation fault`ом.
+     */
+    
     for (; i < tokensCount; i++)
         free (tokens [i]);
     free (tokens);
+    /*
+     * FIXIT:
+     * Освободите уж всю память, которую выделили
+     */
     return 0;
-
 }
